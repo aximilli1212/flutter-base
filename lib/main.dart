d052b0c2c3e0bc7b2067f3aeca55ecdf3df4438a
Aximilli1212 <aximill1212@gmail.com>
2020-03-12 14:12:57 +0000

    add new app icons for android and ios

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,14 @@ import 'package:flutter/material.dart';
 void main() {runApp(
   MaterialApp(
     home: Scaffold(
-      backgroundColor: Colors.blueGrey[200],
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text('I am Rich'),
         backgroundColor: Colors.blueGrey[900],
       ),
       body: Center(
         child:Image(
-          image:NetworkImage('https://ya-webdesign.com/images250_/chick-transparent-day-old-1.png'),
+          image:AssetImage('images/chicka.png'),
         ),
       )
     )
